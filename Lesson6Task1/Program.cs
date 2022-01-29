f2b35b5a7b7319aa4ac453d40bca85212ec0f4ee
Алексей Кузнецов <89538588008@mail.ru>
2022-01-29 13:01:55 +0700

    Добавьте файлы проекта.

---
+++ Lesson6Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение без знаков препинания: ");
+            string sentence = Console.ReadLine();
+            string[] word = sentence.Split(new Char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+            int maxlength = 0, index = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i].Length > maxlength)
+                {
+                    maxlength = word[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", word[index]);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lesson6Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст для проверки является ли он палиндромом: ");
+            string text = Console.ReadLine();
+            text = text.Replace(" ", string.Empty);
+            text = text.ToLower();
+            char[] obrtext = text.ToCharArray();
+            Array.Reverse(obrtext);
+            string finaltext = new string(obrtext);
+            if (text == finaltext)
+            {
+                Console.Write("является");
+            }
+            if (text != finaltext)
+            {
+                Console.Write("не является!");
+                           }
+            Console.ReadKey();
+        }
+    }
+}
